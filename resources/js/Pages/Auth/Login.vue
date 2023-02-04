ce3c1b24a468f3b623453979aad0e4409f1d9af3
André Domingues <dominguesstroppa@gmail.com>
2023-02-04 12:51:39 -0300

    Sistema de login concluido, customizações de cores para as da marca feito

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -30,6 +30,7 @@ const submit = () => {
 </script>
 
 <template>
+    <div class="bg-th-black-900">
     <Head title="Log in" />
 
     <AuthenticationCard>
@@ -40,8 +41,7 @@ const submit = () => {
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
             {{ status }}
         </div>
-
-        <form @submit.prevent="submit">
+        <form @submit.prevent="submit" class="bg-th-blue-700">
             <div>
                 <InputLabel for="email" value="Email" />
                 <TextInput
@@ -54,9 +54,8 @@ const submit = () => {
                 />
                 <InputError class="mt-2" :message="form.errors.email" />
             </div>
-
             <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+                <InputLabel for="password" value="Senha" />
                 <TextInput
                     id="password"
                     v-model="form.password"
@@ -71,19 +70,19 @@ const submit = () => {
             <div class="block mt-4">
                 <label class="flex items-center">
                     <Checkbox v-model:checked="form.remember" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                    <span class="ml-2 text-sm text-th-white font-bold">Manter Conectado</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Forgot your password?
-                </Link>
-
                 <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Log in
                 </PrimaryButton>
             </div>
         </form>
     </AuthenticationCard>
+    </div>
 </template>
+<style scoped>
+
+</style>
